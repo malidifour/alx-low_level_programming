5075689def220dfa754a7a7bcce74019bdbde3b0
malidifour <malidifour@gmail.com>
2022-03-24 08:04:44 -0700

    pointers arrray strings adv

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-* _strncat - concatenates two strings
-* @dest: destination pointer
-* @src: source pointer
-* @n: interger
-* Return: pointer to the string in dest
-*/
+ * _strncat - concatenates strings
+ * @dest: destination
+ * @src: source
+ * @n: integer
+ * Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	
 	int c1 = 0, c2 = 0;
 
 	while (*(dest + c1) != '\0')
