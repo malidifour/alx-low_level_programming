c36fb44559043ca16fb048bd2f04935591347725
malidifour <malidifour@gmail.com>
2022-04-26 15:21:36 -0700

    del node head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * pop_listint - prints value in head and deletes it
+ * @head: pointer to pointer to head
+ * Return: the value in head before it's deleted
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *node = *head;
+
+	if (*head == NULL)
+		return (0);
+	n = node->n;
+	*head = node->next;
+	free(node);
+	return (n);
+}
