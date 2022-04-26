b4cbfc434a64afbf86426d840c7a2f5338462513
malidifour <malidifour@gmail.com>
2022-04-26 12:26:59 -0700

    size_len

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - prints number of elements
+ * @h: pointer to the list
+ * Return: Number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
