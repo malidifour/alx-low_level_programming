09f0b39e6c8c07e10a589ba56e0c5cac1e667250
malidifour <malidifour@gmail.com>
2022-04-27 15:02:34 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,31 +1,51 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 /**
- * print_strings - prints strings
- * @separator: printed between strings
- * @n: number of arguments passed in
- * Return: Nothing.
+ * print_all - prints everything
+ * @format: pointer to a character that
+ * will determine what is to be printed
+ * based on cfis.
+ * Return: nothing
  */
-
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_all(const char * const format, ...)
 {
 	va_list arglist;
-	unsigned int i;
+	unsigned int i = 0, j, k = 0;
 	char *str;
+	const char args[] = "cifs";
 
-	va_start(arglist, n);
-
-	for (i = 0; i < n; i++)
+	va_start(arglist, format);
+	while (format && format[i])
 	{
-		str = va_arg(arglist, char *);
-		if (str)
+		j = 0;
+		while (args[j])
+		{
+			if (format[i] == args[j] && k)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(arglist, int)), k = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(arglist, int)), k = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(arglist, double)), k = 1;
+			break;
+		case 's':
+			str = va_arg(arglist, char *), k = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
 			printf("%s", str);
-		else
-			printf("(nil)");
-		if (i < n - 1)
-			if (separator)
-				printf("%s", separator);
+			break;
+		} i++;
 	}
-	printf("\n");
-	va_end(arglist);
+	printf("\n"), va_end(arglist);
 }
